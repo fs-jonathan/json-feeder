dd55300d6d80d25fad839c28e27384ee133ef53e
Jonathan Yeung <yo@fullspeed.co.jp>
2018-10-25 10:23:03 +0900

    データと入り口調整

--- main.go
+++ main.go
@@ -22,12 +22,17 @@ func main() {
 		return
 	}
 
-	http.HandleFunc("/", jsonWriter)
+	http.HandleFunc("/getJson", jsonWriter)
 	http.ListenAndServe(":" + port, nil)
 }
 
 func jsonWriter(w http.ResponseWriter, r *http.Request) {
-	record := Record{1, "本日（現時点まで）", "先週の同じ曜日との比較", 9, 8, 7}
+	record := []Record{}
+	record.append(record, Record{1, "本日（現時点まで）", "", 9, 8, 7})
+	record.append(record, Record{2, "昨日", "先週の同じ曜日との比較", 344, 43243, 43})
+	record.append(record, Record{3, "今月（現時点まで）", "先週の同じ曜日との比較", 1, 0, 0})
+	record.append(record, Record{4, "先月", "先々月との比較", 93, 83, 72})
+	record.append(record, Record{5, "全期間", "", 20, 4, 434})
 
 	js, err := json.Marshal(record)
 	if err != nil {
