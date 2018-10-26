5f709df8e56ef84a7c2a45ddfda6673ca420e560
Jonathan Yeung <yo@fullspeed.co.jp>
2018-10-26 11:26:06 +0900

    元の機能復帰

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 
-	_ "github.com/labstack/echo"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type Record struct {
@@ -24,51 +22,39 @@ type Message struct {
 }
 
 func main() {
-	port := os.Getenv("PORT")
+	// Echo instance
+	e := echo.New()
 
-	if port == "" {
-		return
-	}
+	// middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	http.HandleFunc("/", blankScreen)
-	http.HandleFunc("/getJson", jsonWriter)
-	http.ListenAndServe(":"+port, nil)
+	// Route => handler
+	e.GET("/", blankScreen)
+	e.POST("/getJson", jsonWriter)
+
+	// Start server from PORT number
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
 
-func jsonWriter(w http.ResponseWriter, r *http.Request) {
+func jsonWriter(c echo.Context) error {
+	// Receive message
+	message := new(Message)
+
+	if reqErr := c.Bind(message); reqErr != nil {
+		return c.String(http.StatusBadRequest, reqErr.Error())
+	}
+
+	// Return message
 	s0 := []Record{Record{1, "本日（現時点まで）", "", 9, 8, 7}}
 	s1 := append(s0, Record{2, "昨日", "先週の同じ曜日との比較", 344, 43243, 43})
 	s2 := append(s1, Record{3, "今月（現時点まで）", "先週の同じ曜日との比較", 1, 0, 0})
 	s3 := append(s2, Record{4, "先月", "先々月との比較", 93, 83, 72})
-	record := append(s3, Record{5, "全期間", "", 20, 4, 434})
+	records := append(s3, Record{5, "全期間", "", 20, 4, 434})
 
-	js, err := json.Marshal(record)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	b, parseErr := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-
-	if parseErr != nil {
-		log.Printf("Parse Error")
-	}
-
-	data := Message{}
-	decodeErr := json.Unmarshal(b, &data)
-
-	if decodeErr != nil {
-		log.Printf("Decode Error")
-	}
-
-	log.Printf("%s", b)
-	log.Printf(string(js))
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	return c.JSON(http.StatusOK, records)
 }
 
-func blankScreen(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "hello.html")
+func blankScreen(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, Ready!!")
 }
